e32d04d90e3577e8d237392203e33c31f0371c5e
lal-kablan <lalkablan@college.harvard.edu>
2023-06-16 13:50:14 -0400

    Help file for bosTrees

--- R/bosTrees.R
+++ R/bosTrees.R
@@ -2,10 +2,33 @@
 #'
 #' Includes data for street trees in Boston. Provided by Boston's GIS department.
 #'
-#' @format A tibble with XXX rows and XXX variables:
+#' @format A tibble with 6836 rows and 25 variables:
 #' \describe{
 #'   \item{FID}{unique identifier for each tree}
 #'   \item{Species}{a character denoting species}
+#'   \item{A}{xxx}
+#'   \item{Address}{The Boston address that the tree is at}
+#'   \item{Notes}{Notes for finding the tree}
+#'   \item{Season}{The season in which the tree was recorded}
+#'   \item{Inspected}{A binary for whether or not the tree was inspected}
+#'   \item{Alive}{A binary for whether or not the tree is alive}
+#'   \item{Leaning}{XXX}
+#'   \item{TreeThere}{XXX}
+#'   \item{Flag}{XXX}
+#'   \item{Info}{XXX}
+#'   \item{Longitude}{Longitude information}
+#'   \item{Latitude}{Latitude information}
+#'   \item{Watered}{A binary for whether or not the tree is watered}
+#'   \item{GlobalID}{XXX}
+#'   \item{CreationDa}{XXX}
+#'   \item{Creator}{XXX}
+#'   \item{EditDate}{XXX}
+#'   \item{Editor}{XXX}
+#'   \item{GlobalID2}{XXX}
+#'   \item{CreationDate}{XXX}
+#'   \item{Creator_1}{XXX}
+#'   \item{EditDate_1}{XXX}
+#'   \item{Editor_1}{XXX}
 #'
 #' }
 #' @source{https://data.boston.gov/dataset/primary-street-trees-public}
